af863e62e2df7d17b1b8905d98ed7362291a4980
Nikansha Maharaj <nikansha.m@gmail.com>
2022-06-06 13:57:55 -0700

    Converted the create view (def create_recipe) to a list (RecipeCreateView) for a Recipe

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,17 +2,17 @@ from django.urls import path
 
 
 from recipes.views import (
-    create_recipe,
     change_recipe,
     log_rating,
     RecipeListView,
     RecipeDetailView,
+    RecipeCreateView,
 )
 
 urlpatterns = [
     path("", RecipeListView.as_view(), name="recipes_list"),
     path("<int:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
-    path("new/", create_recipe, name="recipe_new"),
+    path("new/", RecipeCreateView.as_view(), name="recipe_new"),
     path("edit/", change_recipe, name="recipe_edit"),
     path("<int:recipe_id>/ratings/", log_rating, name="recipe_rating"),
 ]
